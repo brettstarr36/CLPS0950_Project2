4e8c43cd544b2acfab6d584f22d540136f3071f3
JonnyRussell <jonathan_russell@brown.edu>
2022-04-29 10:27:22 -0400

    practice gameplay push

---
+++ practice_game.py
@@ -0,0 +1,7 @@
+print("Welcome to the Color Visual Acuity Game!")  # welcoming message
+print("type 1 to continue")  # engagement to trigger directions
+intro_1 = input()
+if intro_1 == "1":  # confirm engagement
+    print("start game")
+else:
+    print("end game")
