5d15dc2f29fbe72d7ba272f3422d88dca8355a0b
JonnyRussell <jonathan_russell@brown.edu>
2022-05-02 13:54:58 -0400

    practice gameplay converting some of the gameplay updated

--- practice_game.py
+++ practice_game.py
@@ -1,5 +1,10 @@
 print("Welcome to the Color Visual Acuity Game!") # welcoming message
-intro_1 = input("enter 1 to continue ") # engagement to trigger directions
+intro_1 = input("enter 1 to continue")# engagement to trigger directions
 if intro_1 == "1": # confirm engagement
-    print("You will be shown a grid of squares, one of the squares will be a different hue")
-    print("You will be asked to choose the square that is a different hue")
+    print("Directions: You will be shown an image with a grid where")
+    print("all the colors are of the same hue except one square")
+    print("You will be shown a matrix of numbers")
+    print("corresponding to the squares in the image")
+    print("You will be asked to type the number")
+    print("corresponding to the square that is a different hue") # displays instructions to participant
+    intro_2 = input("enter 1 to continue")
