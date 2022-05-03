e6fc269ea744513db4e8f23585143c7079051229
brettstarr36 <brett_starr@brown.edu>
2022-05-03 16:46:42 -0400

    Brett 3-5 pm 5/3/22: added clicking capability

--- practice_display.py
+++ practice_display.py
@@ -1,12 +1,14 @@
-#Brett 2:00 pm - 4:00 pm 5/2/2022
 import pygame
 pygame.init()
 background = pygame.display.set_mode((630, 630))
 base_color = (160, 170, 120)
+alt_color = (160, 180, 120)
 pygame.draw.rect(background, base_color, pygame.Rect((10, 10), (300, 300)))
 pygame.draw.rect(background, base_color, pygame.Rect((320, 10), (300, 300)))
-pygame.draw.rect(background, base_color, pygame.Rect((10, 320), (300, 300)))
 pygame.draw.rect(background, base_color, pygame.Rect((320, 320), (300, 300)))
+button = pygame.Surface((300, 300))
+pygame.Surface.fill(button, alt_color)
+b = background.blit(button, (10, 320))
 pygame.display.flip()
 running = True
 while running:
@@ -15,3 +17,7 @@ while running:
             running = False
         if running == False:
             pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pos = pygame.mouse.get_pos()
+            if b.collidepoint(pos):
+                pygame.quit()
