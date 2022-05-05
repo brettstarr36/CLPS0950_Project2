c58d9c5f924f4e4b6b4abdfa1b3de0f12e98071e
brettstarr36 <brett_starr@brown.edu>
2022-05-05 14:30:09 -0400

    Brett, Lily and Jonny 12-3 pm 5/4/22: sorting out repeating levels

--- making_loop.py
+++ making_loop.py
@@ -1,12 +1,25 @@
 import pygame
 import random
 pygame.init()
+score = 0
 background = pygame.display.set_mode((670, 670))
 R_value = random.randint(0,255)
 G_value = random.randint(0,255)
 B_value = random.randint(0,255)
 base_color = (R_value, G_value, B_value)
-alt_color = ((R_value + 10), (G_value + 10), (B_value + 10))
+if R_value > 205:
+    R_val_alt = R_value - 50
+else:
+    R_val_alt = R_value + 50
+if G_value > 205:
+    G_val_alt = G_value - 50
+else:
+    G_val_alt = G_value + 50
+if B_value > 205:
+    B_val_alt = B_value - 50
+else:
+    B_val_alt = B_value + 50
+alt_color = (R_val_alt, G_val_alt, B_val_alt)
 
 for n in range(0,6):
     for j in range(0,6):
@@ -19,7 +32,6 @@ pygame.Surface.fill(button, alt_color)
 b = background.blit(button, (((100*j_cord) + 10*(j_cord+1)), ((100*n_cord) + 10*(n_cord+1))))
 pygame.display.flip()
 running = True
-score = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,5 +42,37 @@ while running:
             pos = pygame.mouse.get_pos()
             if b.collidepoint(pos): #If they click the right button
                 score += 1
+                R_value = random.randint(0, 255)
+                G_value = random.randint(0, 255)
+                B_value = random.randint(0, 255)
+                base_color = (R_value, G_value, B_value)
+                if R_value > 205:
+                    R_val_alt = R_value - 50
+                else:
+                    R_val_alt = R_value + 50
+                if G_value > 205:
+                    G_val_alt = G_value - 50
+                else:
+                    G_val_alt = G_value + 50
+                if B_value > 205:
+                    B_val_alt = B_value - 50
+                else:
+                    B_val_alt = B_value + 50
+                alt_color = (R_val_alt, G_val_alt, B_val_alt)
+
+                for n in range(0, 6):
+                    for j in range(0, 6):
+                        pygame.draw.rect(background, base_color,
+                                         pygame.Rect((((100 * j) + 10 * (j + 1)), ((100 * n) + 10 * (n + 1))),
+                                                     (100, 100)))
+
+                n_cord = random.randint(0, 5)
+                j_cord = random.randint(0, 5)
+                button = pygame.Surface((100, 100))
+                pygame.Surface.fill(button, alt_color)
+                b = background.blit(button,
+                                    (((100 * j_cord) + 10 * (j_cord + 1)), ((100 * n_cord) + 10 * (n_cord + 1))))
+                pygame.display.flip()
             else: #if they click the wrong button
                 pygame.quit()
+                print('Your score was', score)
